ca687895623b0238ef3df97594b2998ad46f64b6
ShravanJadhav <shravanjadhav844@gmail.com>
2024-10-22 00:30:42 +0530

    logging and exception

---
+++ app.py
@@ -0,0 +1,10 @@
+from src.StudentAnalysis.logger import logging
+from src.StudentAnalysis.exception import CustomException
+import sys
+if __name__=="__main__":
+    logging.info("The execution has started")
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info("Custom Exception")
+        raise CustomException(e,sys)
